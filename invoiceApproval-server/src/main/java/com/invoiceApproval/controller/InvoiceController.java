c74477c238393943adb519e404274d4e6e68a96b
atul_jadhav <atul_jadhav@persistent.com>
2019-03-06 13:45:51 +0530

    invoice Input

---
+++ invoiceApproval-server/src/main/java/com/invoiceApproval/controller/InvoiceController.java
@@ -0,0 +1,46 @@
+package com.invoiceApproval.controller;
+
+import javax.validation.Valid;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.invoiceApproval.Utils.Constants;
+import com.invoiceApproval.Utils.Messages;
+import com.invoiceApproval.entity.Invoice;
+import com.invoiceApproval.entity.ResponseVO;
+import com.invoiceApproval.service.impl.InvoiceService;
+
+@RestController
+public class InvoiceController {
+
+	private static final Logger logger = LogManager.getLogger(InvoiceService.class);
+	
+	@Autowired
+	private InvoiceService invoiceService;
+	
+	@Autowired
+	private Messages messages;
+
+	/**
+	 * This method is use to save the invoice details into database.
+	 * @param invoice
+	 * @return ResponseVO
+	 */
+	@PostMapping(path="/invoiceInput")
+	public ResponseVO saveInvoiceDetails(@Valid @RequestBody Invoice invoice) {
+		
+		logger.info("calling saveInvoiceDetails() of InvoiceController");
+		try {
+			invoiceService.saveInvoiceDetails(invoice);
+			return new ResponseVO(Constants.SUCCESS, messages.get("invoice.success"),null);
+		} catch (Exception e) {
+			logger.error(messages.get("invoice.error"),e);
+			return new ResponseVO(Constants.FAILED, messages.get("invoice.error"), e.getMessage());
+		}
+	}
+}
